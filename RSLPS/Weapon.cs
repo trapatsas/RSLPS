ed2b903d228a19fe24a797c0493fe617bac48e82
Panayotis Trapatsas <trapatsas@gmail.com>
2014-04-23 21:06:09 +0300

    TO-DO: Add decision table

---
+++ RSLPS/Weapon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSLPS
+{
+    class Weapon
+    {
+        public Weapon(Game.HandWeapons value)
+        {
+            Name = value;
+        }
+        public Game.HandWeapons Name { get; set; }
+
+        public Game.WinningActions Cry { get; set; }
+    }
+}
