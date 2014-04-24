318a79a3a8299bcfc2de979216855134a9f672a6
Panayotis Trapatsas <trapatsas@gmail.com>
2014-04-24 17:12:17 +0300

    v1 complete

--- RSLPS/Game.cs
+++ RSLPS/Game.cs
@@ -29,8 +29,67 @@ namespace RSLPS
 
         public static List<Decision> PossibleOutcomes = new List<Decision>()
             {
-                new Decision { HumanWeapon = Game.HandWeapons.Rock, AIWeapon = Game.HandWeapons.Rock, MatchResult = Game.Results.Tie, TimesPlayed = 0, WeaponCry = Game.WinningActions.tie},
-                new Decision { HumanWeapon = Game.HandWeapons.Paper, AIWeapon = Game.HandWeapons.Lizard, MatchResult = Game.Results.ComputerWin, TimesPlayed = 0, WeaponCry = Game.WinningActions.decapitate}
+                new Decision { HumanWeapon = Game.HandWeapons.Rock, AIWeapon = Game.HandWeapons.Lizard
+                    , MatchResult = Game.Results.HumanWin, TimesPlayed = 0, WeaponCry = Game.WinningActions.crushes},
+                new Decision { HumanWeapon = Game.HandWeapons.Paper, AIWeapon = Game.HandWeapons.Rock
+                    , MatchResult = Game.Results.HumanWin, TimesPlayed = 0, WeaponCry = Game.WinningActions.covers},
+                new Decision { HumanWeapon = Game.HandWeapons.Rock, AIWeapon = Game.HandWeapons.Lizard
+                    , MatchResult = Game.Results.HumanWin, TimesPlayed = 0, WeaponCry = Game.WinningActions.crushes},
+                new Decision { HumanWeapon = Game.HandWeapons.Lizard, AIWeapon = Game.HandWeapons.Spock
+                    , MatchResult = Game.Results.HumanWin, TimesPlayed = 0, WeaponCry = Game.WinningActions.poisons},
+                new Decision { HumanWeapon = Game.HandWeapons.Spock, AIWeapon = Game.HandWeapons.Scissors
+                    , MatchResult = Game.Results.HumanWin, TimesPlayed = 0, WeaponCry = Game.WinningActions.smashes},
+                new Decision { HumanWeapon = Game.HandWeapons.Scissors, AIWeapon = Game.HandWeapons.Lizard
+                    , MatchResult = Game.Results.HumanWin, TimesPlayed = 0, WeaponCry = Game.WinningActions.decapitate},
+                new Decision { HumanWeapon = Game.HandWeapons.Lizard, AIWeapon = Game.HandWeapons.Paper
+                    , MatchResult = Game.Results.HumanWin, TimesPlayed = 0, WeaponCry = Game.WinningActions.eats},
+                new Decision { HumanWeapon = Game.HandWeapons.Paper, AIWeapon = Game.HandWeapons.Spock
+                    , MatchResult = Game.Results.HumanWin, TimesPlayed = 0, WeaponCry = Game.WinningActions.disproves},
+                new Decision { HumanWeapon = Game.HandWeapons.Spock, AIWeapon = Game.HandWeapons.Rock
+                    , MatchResult = Game.Results.HumanWin, TimesPlayed = 0, WeaponCry = Game.WinningActions.vaporizes},
+                new Decision { HumanWeapon = Game.HandWeapons.Rock, AIWeapon = Game.HandWeapons.Scissors
+                    , MatchResult = Game.Results.HumanWin, TimesPlayed = 0, WeaponCry = Game.WinningActions.crushes},
+                new Decision { AIWeapon = Game.HandWeapons.Rock, HumanWeapon = Game.HandWeapons.Lizard
+                    , MatchResult = Game.Results.ComputerWin, TimesPlayed = 0, WeaponCry = Game.WinningActions.crushes},
+                new Decision { AIWeapon = Game.HandWeapons.Paper, HumanWeapon = Game.HandWeapons.Rock
+                    , MatchResult = Game.Results.ComputerWin, TimesPlayed = 0, WeaponCry = Game.WinningActions.covers},
+                new Decision { AIWeapon = Game.HandWeapons.Rock, HumanWeapon = Game.HandWeapons.Lizard
+                    , MatchResult = Game.Results.ComputerWin, TimesPlayed = 0, WeaponCry = Game.WinningActions.crushes},
+                new Decision { AIWeapon = Game.HandWeapons.Lizard, HumanWeapon = Game.HandWeapons.Spock
+                    , MatchResult = Game.Results.ComputerWin, TimesPlayed = 0, WeaponCry = Game.WinningActions.poisons},
+                new Decision { AIWeapon = Game.HandWeapons.Spock, HumanWeapon = Game.HandWeapons.Scissors
+                    , MatchResult = Game.Results.ComputerWin, TimesPlayed = 0, WeaponCry = Game.WinningActions.smashes},
+                new Decision { AIWeapon = Game.HandWeapons.Scissors, HumanWeapon = Game.HandWeapons.Lizard
+                    , MatchResult = Game.Results.ComputerWin, TimesPlayed = 0, WeaponCry = Game.WinningActions.decapitate},
+                new Decision { AIWeapon = Game.HandWeapons.Lizard, HumanWeapon = Game.HandWeapons.Paper
+                    , MatchResult = Game.Results.ComputerWin, TimesPlayed = 0, WeaponCry = Game.WinningActions.eats},
+                new Decision { AIWeapon = Game.HandWeapons.Paper, HumanWeapon = Game.HandWeapons.Spock
+                    , MatchResult = Game.Results.ComputerWin, TimesPlayed = 0, WeaponCry = Game.WinningActions.disproves},
+                new Decision { AIWeapon = Game.HandWeapons.Spock, HumanWeapon = Game.HandWeapons.Rock
+                    , MatchResult = Game.Results.ComputerWin, TimesPlayed = 0, WeaponCry = Game.WinningActions.vaporizes},
+                new Decision { AIWeapon = Game.HandWeapons.Rock, HumanWeapon = Game.HandWeapons.Scissors
+                    , MatchResult = Game.Results.Tie, TimesPlayed = 0, WeaponCry = Game.WinningActions.crushes},
+                new Decision { AIWeapon = Game.HandWeapons.Scissors, HumanWeapon = Game.HandWeapons.Scissors
+                    , MatchResult = Game.Results.Tie, TimesPlayed = 0, WeaponCry = Game.WinningActions.tie},
+                new Decision { AIWeapon = Game.HandWeapons.Lizard, HumanWeapon = Game.HandWeapons.Lizard
+                    , MatchResult = Game.Results.Tie, TimesPlayed = 0, WeaponCry = Game.WinningActions.tie},
+                new Decision { AIWeapon = Game.HandWeapons.Paper, HumanWeapon = Game.HandWeapons.Paper
+                    , MatchResult = Game.Results.Tie, TimesPlayed = 0, WeaponCry = Game.WinningActions.tie},
+                new Decision { AIWeapon = Game.HandWeapons.Spock, HumanWeapon = Game.HandWeapons.Spock
+                    , MatchResult = Game.Results.Tie, TimesPlayed = 0, WeaponCry = Game.WinningActions.tie},
+                new Decision { AIWeapon = Game.HandWeapons.Rock, HumanWeapon = Game.HandWeapons.Rock
+                    , MatchResult = Game.Results.Tie, TimesPlayed = 0, WeaponCry = Game.WinningActions.tie}
             };
     }
 }
+
+//Scissors cut paper
+//Paper covers rock
+//Rock crushes lizard
+//Lizard poisons Spock
+//Spock smashes scissors
+//Scissors decapitate lizard
+//Lizard eats paper
+//Paper disproves Spock
+//Spock vaporizes rock
+//Rock crushes scissors
--- RSLPS/Program.cs
+++ RSLPS/Program.cs
@@ -14,11 +14,12 @@ namespace RSLPS
             while (input.ToUpper() != "Q")
             {
                 Console.WriteLine("");
-                Console.WriteLine("╔======================== New game round {0} ============================╗", Game.TotalGames + 1);
-                Console.WriteLine("║ Please select a weapon                                               ║");
-                Console.WriteLine("║ Rock (R)- Paper (P)- Scissors(Sc) - Lizard (Li) - Spock (Sp)         ║");
-                Console.WriteLine("║ or type \"Q\" to quit!                                                 ║");
-                Console.WriteLine("╚======================================================================╝");
+                Console.WriteLine("╔============== New game round {0} =============╗", Game.TotalGames + 1);
+                Console.WriteLine("║ Please select a weapon:                       ║");
+                Console.WriteLine("║ 1. Rock (R) - 2. Paper (P) - 3. Scissors(Sc)  ║");
+                Console.WriteLine("║ 4. Lizard (Li) - 5. Spock (Sp)                ║");
+                Console.WriteLine("║ or type \"Q\" to quit!                        ║");
+                Console.WriteLine("╚===============================================╝");
                 Console.WriteLine("");
                 Console.WriteLine("Type your choice: ");
                 Console.WriteLine("");
@@ -27,30 +28,36 @@ namespace RSLPS
 
                 switch (input.ToUpper()) //Switch on Key enum
                 {
+                    case "0":
                     case "Q":
                     case "QUIT":
                     case "EXIT":
                         input = "Q";
                         break;
+                    case "1":
                     case "R":
                     case "RO":
                     case "ROCK":
                         userChoice = new Weapon(Game.HandWeapons.Rock);
                         break;
+                    case "3":
                     case "SC":
                     case "SCISSORS":
                         userChoice = new Weapon(Game.HandWeapons.Scissors);
                         break;
+                    case "4":
                     case "L":
                     case "LI":
                     case "LIZARD":
                         userChoice = new Weapon(Game.HandWeapons.Lizard);
                         break;
+                    case "2":
                     case "P":
                     case "PA":
                     case "PAPER":
                         userChoice = new Weapon(Game.HandWeapons.Paper);
                         break;
+                    case "5":
                     case "SP":
                     case "SPOCK":
                         userChoice = new Weapon(Game.HandWeapons.Spock);
@@ -76,11 +83,11 @@ namespace RSLPS
             {
                 case Game.Results.ComputerWin:
                     Game.ComputerWins++;
-                    winner = string.Format("║ {0} does {1}. You Lost!", newRound.ComputerWeapon.Name, userChoice.Name);
+                    winner = string.Format("║ {0} {2} {1}. You Lost!", newRound.ComputerWeapon.Name, userChoice.Name, newRound.WeaponCry);
                     break;
                 case Game.Results.HumanWin:
                     Game.HumanWins++;
-                    winner = string.Format("║ {1} does {0}. You Win!", newRound.ComputerWeapon.Name, userChoice.Name);
+                    winner = string.Format("║ {1} {2} {0}. You Win!", newRound.ComputerWeapon.Name, userChoice.Name, newRound.WeaponCry);
                     break;
                 case Game.Results.Tie:
                     Game.Ties++;
